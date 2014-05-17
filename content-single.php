8b1dc360359712641b60b5fd0dc8e659b0bf26a1
metodiew <stanko@metodiew.com>
2014-05-17 04:10:19 +0300

    Several updates for featured images, comments and yarpp

--- content-single.php
+++ content-single.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @package A Piece Of Cake
+ * @since available since Release 1.0
  */
 ?>
 
@@ -13,10 +14,10 @@
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 	
-	<?php 
+	<?php
 	if ( has_post_thumbnail() ) {
 		echo '<div class="apoc-featured-image">';
-			the_post_thumbnail( array(1000, 500), array( 'class' => 'aligncenter' ) );
+			the_post_thumbnail( 'apoc-thumbnail', array( 'class' => 'aligncenter' ) );
 		echo '</div>';
 	}
 	?>
@@ -36,33 +37,33 @@
 	<footer class="entry-footer">
 		<?php
 			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'apoc' ) );
+			$apoc_category_list = get_the_category_list( __( ', ', 'apoc' ) );
 
 			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'apoc' ) );
+			$apoc_tag_list = get_the_tag_list( '', __( ', ', 'apoc' ) );
 
 			if ( ! apoc_categorized_blog() ) {
 				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
+				if ( '' != $apoc_tag_list ) {
+					$apoc_meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
 				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
+					$apoc_meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
 				}
 
 			} else {
 				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
+				if ( '' != $apoc_tag_list ) {
+					$apoc_meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
 				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
+					$apoc_meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'apoc' );
 				}
 
 			} // end check for categories on this blog
 
 			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
+				$apoc_meta_text,
+				$apoc_category_list,
+				$apoc_tag_list,
 				get_permalink()
 			);
 		?>
--- functions.php
+++ functions.php
@@ -6,4 +6,5 @@ function apoc_child_scripts() {
 
 	wp_enqueue_style( 'apoc-style-child', get_stylesheet_directory_uri() . '/style.css' );
 }
+
 add_action( 'wp_enqueue_scripts', 'apoc_child_scripts' );
